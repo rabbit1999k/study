f52361e45d8c5f2be15a52747fa81f00303ccd87
unknown <rabbit1999k@gmail.com>
2021-08-02 00:50:13 +0900

    end

--- tetris/tetris.cpp
+++ tetris/tetris.cpp
@@ -17,12 +17,21 @@ using namespace std;
 #define DOWN 80
 
 enum state {
-	WALL = 1,
+	EMPTY,
+	WALL,
 	BLOCK,
 	TOUCH,
 	FLOOR
 };
 
+void CursorView(char show) {
+	HANDLE hConsole;
+	CONSOLE_CURSOR_INFO ConsoleCursor;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	ConsoleCursor.bVisible = show;
+	ConsoleCursor.dwSize = 1;
+	SetConsoleCursorInfo(hConsole, &ConsoleCursor);
+}
 void gotoxy(int x, int y) {
 	COORD pos;
 	pos.X = 2 * x;
@@ -187,7 +196,7 @@ public:
 	}
 
 	void down() {
-		y--;
+		y++;
 	}
 	void left() {
 		x--;
@@ -287,6 +296,26 @@ public:
 	}
 };
 
+class backup {
+public:
+	static void updateBlock(Block* source, Block& backupBlock) {
+		backupBlock.setX(source->getX()); 
+		backupBlock.setY(source->getY()); 
+		backupBlock.setRCnt(source->getRCnt());
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				for (int k = 0; k < 4; k++) {
+					backupBlock.setShape(i, j, k, source->getShape(i, j, k)); // 블럭의 모양 백업
+				}
+			}
+		}
+	}
+	static void updateTable(vector<vector<int> >& source, vector<vector<int> >& backupTable) {
+		backupTable.resize(source.size(), vector<int>(source.size())); // 기존 table의 크기만큼 2차원 벡터 크기 할당
+		copy(source.begin(), source.end(), backupTable.begin());
+	}
+};
+
 class game {
 private:
 	int x;
@@ -324,13 +353,205 @@ public:
 	void drawTable() {
 		for (int i = 0; i < y; i++) {
 			for (int j = 0; j < x; j++) {
+				if (table[i][j] == WALL || table[i][j] == FLOOR)
+					cout << "▩";
+				else if (table[i][j] == BLOCK || table[i][j] == TOUCH)
+					cout << "■";
+				else
+					cout << "  ";
+			}
+			cout << '\n';
+		}
+	}
 
+	void createBlock() {
+		srand((unsigned int)time(NULL));
+		int sel = rand() % 5 + 1;
+		switch (sel) {
+		case 1:
+			block = new Block1();
+			break;
+		case 2:
+			block = new Block2();
+			break;
+		case 3:
+			block = new Block3();
+			break;
+		case 4:
+			block = new Block4();
+			break;
+		case 5:
+			block = new Block5();
+			break;
+		}
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				int y = j + block->getY();
+				int x = i + block->getX();
+				table[y][x] = block->getShape(block->getRCnt(), i, j);
 			}
 		}
 	}
 
+	void move(int direct) {
+		Block backupB;
+		vector<vector<int>> backupT;
+		backup::updateBlock(block, backupB);
+		backup::updateTable(table, backupT);
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				int Y = j + block->getY();
+				int X = i + block->getX();
+				if (table[Y][X] == BLOCK) //블럭 테이블에서 삭제
+					table[Y][X] = EMPTY;
+			}
+		}
+		if (direct == DOWN) {
+			block->down();
+		}
+		else if (direct == LEFT) {
+			block->left();
+		}
+		else if (direct == RIGHT) {
+			block->right();
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				int Y = j + block->getY();
+				int X = i + block->getX();
+				int blockValue = block->getShape(block->getRCnt(), i, j);
+				if (blockValue != BLOCK) continue;
+				if (table[Y][X] == EMPTY) table[Y][X] = blockValue;
+				else if (table[Y][X] == WALL) {
+					copy(backupT.begin(), backupT.end(), table.begin());
+					block->setX(backupB.getX());
+					block->setY(backupB.getY());
+					return;
+				}
+				else if (table[Y][X] == TOUCH) {
+					copy(backupT.begin(), backupT.end(), table.begin());
+					block->setX(backupB.getX());
+					block->setY(backupB.getY());
+					if (direct == DOWN) {
+						build();
+						createBlock();
+					}
+					return;
+				}
+				else if (table[Y][X] == FLOOR) {
+					copy(backupT.begin(), backupT.end(), table.begin());
+					block->setX(backupB.getX());
+					block->setY(backupB.getY());
+					if (direct == DOWN) {
+						build();
+						createBlock();
+					}
+					return;
+				}
+			}
+		}
+	}
+
+	void build() {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				int Y = j + block->getY();
+				int X = i + block->getX();
+				int blockValue = block->getShape(block->getRCnt(), i, j);
+				if (blockValue != BLOCK) continue;
+				table[Y][X] = TOUCH;
+			}
+		}
+	}
+
+	void rotateBlock() {
+		Block backupB;
+		vector<vector<int>> backupT;
+		backup::updateBlock(block, backupB);
+		backup::updateTable(table, backupT);
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				int Y = j + block->getY();
+				int X = i + block->getX();
+				if (table[Y][X] == BLOCK) //블럭 테이블에서 삭제
+					table[Y][X] = EMPTY;
+			}
+		}
+		block->rotate();
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				int Y = j + block->getY();
+				int X = i + block->getX();
+				int blockValue = block->getShape(block->getRCnt(), i, j);
+				if (blockValue != BLOCK) continue;
+				else if (blockValue == EMPTY)
+					table[Y][X] = blockValue;
+				else if (table[Y][X] == WALL || table[Y][X] == TOUCH || table[Y][X] == FLOOR) {
+					copy(backupT.begin(), backupT.end(), table.begin());
+					block->setRCnt(backupB.getRCnt());
+					return;
+				}
+			}
+		}
+	}
+
+	
+};
+
+class menu {
+public:
+	menu() {
+		getchar();
+		system("cls");
+	}
+};
+
+class start {
+private:
+	game* g;
+public:
+	start() {
+		g = new game(T_X, T_Y);
+		g->createBlock();
+		g->drawTable();
+		while (1) {
+			int sel;
+			if (_kbhit()) {
+				sel = _getch();
+				if (sel == 224) {
+					sel = _getch();
+					switch (sel) {
+					case UP:
+						g->rotateBlock();
+						break;
+					case DOWN:
+						g->move(DOWN);
+						break;
+					case LEFT:
+						g->move(LEFT);
+						break;
+					case RIGHT:
+						g->move(RIGHT);
+						break;
+					default:
+						break;
+					}
+				}
+			}
+			gotoxy(0, 0);
+			g->drawTable();
+		}
+	}
 };
 
 int main() {
+	CursorView(false);
+	game g(T_X, T_Y);
+	//menu();
+	start();
+	getchar();
 	return 0;
 }
